81f696f75e0c3f58fae99a53eb98cd263ce1e6a5
Daniel Radaideh <mohammadxradaideh@gmail.com>
2024-07-10 17:50:09 +0300

    data to js object

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+import { quizzes } from "./data.js";
+
+console.log(quizzes);
+
 const btnTheme = document.getElementById("theme-switch");
 const bodyElement = document.querySelector("body");
 const iconSun = document.getElementById("sun-icon");
 const iconMoon = document.getElementById("moon-icon");
+const btnHTML = document.getElementById("HTML");
+const btnCSS = document.getElementById("CSS");
+const btnJs = document.getElementById("JavaScript");
+const btnAccess = document.getElementById("Accessibility");
+const mainElement = document.querySelector("main");
+const navImg = document.getElementById("navigation-topic-img");
+const navText = document.getElementById("navigation-topic-text");
+const topicButtons = [btnHTML, btnCSS, btnJs, btnAccess];
+
+// THEME FUNCTIONS
 
 function handleTheme() {
   bodyElement.classList.toggle("dark");
@@ -16,3 +30,16 @@ function handleTheme() {
 }
 
 btnTheme.addEventListener("click", handleTheme);
+
+// TOPIC SELECTION
+
+function handleTopicSelection(topicId) {
+  let topicIndex;
+  if (topicId === "HTML") {
+  }
+  mainElement.innerHTML = "";
+}
+
+topicButtons.forEach((btn) => {
+  btn.addEventListener("click", () => handleTopicSelection(btn.id));
+});
